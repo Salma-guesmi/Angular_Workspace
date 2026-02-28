ac022d0bdebbe6eb4eeaac29d5a40a3e55b245e9
Ons Ben Amor <ons.benamor@esprit.tn>
2026-02-28 13:32:05 +0100

    Exemple fait en class

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+v1 ="Bonjour tout le monde";
+v2="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRGakkUEbXyfann4M16v9CV-sTa915cUOdh9g&s"
+v3="ons"
+fn(){
+  alert("Hello");}
+
+v4=10
+students=['ons','sami','yazid']
+students2=[
+  {id:1, name:'ons', age:22},
+  {id:2, name:'sami', age:23},
+  {id:3, name:'yazid', age:24}
+]
+}
